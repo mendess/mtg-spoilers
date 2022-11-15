be4235150c6f85bfaecd189d79a84869df45284e
mendess <pedro.mendes.26@gmail.com>
2022-11-15 13:09:35 +0200

    impl some traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,15 @@
 pub mod cache;
 pub mod mythic;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct SpoilerSource {
     pub name: String,
     pub url: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Spoiler {
     pub name: Option<String>,
     pub image: String,
     pub source: Option<SpoilerSource>,
 }
-
